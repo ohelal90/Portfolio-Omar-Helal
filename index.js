75737b19d3420e012b94b8c39370ea86b1ce08bd
Omar Helal <omsy09@hotmail.com>
2018-06-28 23:21:08 -0500

    Javascript greet user 06/28/18

---
+++ index.js
@@ -0,0 +1,12 @@
+var greetUser = function(){
+    var userName = prompt("What is your name?");
+    
+    if(userName !== ""){
+    alert("Hello " + userName);
+    } else {
+        greetUser();
+    
+        }
+    };
+
+    greetUser();  
